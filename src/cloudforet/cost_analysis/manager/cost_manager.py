4e1415d5d596e899c2bb0bfc26e98574a5218e05
jihyungSong <bluese@megazone.com>
2023-07-13 11:07:22 +0900

    chore: add logger for debug

--- src/cloudforet/cost_analysis/manager/cost_manager.py
+++ src/cloudforet/cost_analysis/manager/cost_manager.py
@@ -23,8 +23,11 @@ class CostManager(BaseManager):
         end = datetime.utcnow().strftime('%Y-%m-%d')
         target_regions = self.get_region_list(account_id, start, end)
 
+        print(f'[get_data] target_regions: {target_regions}]')
+
         for region_code in target_regions:
             cost_region_query = self._set_query(account_id, region_code, start, end)
+            print(f'[get_data] cost_region_query: {cost_region_query}]')
             response_stream = self.aws_ce_connector.get_cost_and_usage(**cost_region_query)
             for results in response_stream:
                 yield self._make_cost_data(results, account_id, region_code)
@@ -91,6 +94,8 @@ class CostManager(BaseManager):
 
     def get_region_list(self, account_id, start, end):
         region_query = self._set_region_query(account_id, start, end)
+
+        print(f'[get_region_list] region_query: {region_query}]')
 
         azs = []
         for results in self.aws_ce_connector.get_cost_and_usage(**region_query):
