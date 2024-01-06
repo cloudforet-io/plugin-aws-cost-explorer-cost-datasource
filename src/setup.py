eae6ce4c2ea871a5c8231720a5e6223c109e1941
seolmin <seolmin@megazone.com>
2024-01-06 15:55:40 +0900

    build: change build files

--- src/setup.py
+++ src/setup.py
@@ -31,10 +31,8 @@ setup(
     license='Apache License 2.0',
     packages=find_packages(),
     install_requires=[
-        'spaceone-core',
         'spaceone-api',
         'schematics',
-        'boto3',
         'pytz'
     ],
     zip_safe=False,
